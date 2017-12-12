180292bc6cd8578a41e563dd2da35a538f3a6c8a
15517127859@163.com <chenyang@chenyang.com>
2017-12-12 15:00:23 +0800

    homeworks

---
+++ 模块二/day08/res.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# AUTHER   @ Alvin
+
+
+import re
+
+s = "adsd4d41s\\a4f1f1d3f1df3a41d64d63"
+
+print(re.split("\\\\", s))
+
+
+
---
+++ 模块二/homework/ATMofCart/Cart.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# AUTHER   @ Alvin
+
+import http.cookies
+
+class ages:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        self.fors(self.name, self.age)
+
+    def fors(self,name,age):
+
+        print(name, age)
+
+
+ages("Alvin", 22)
